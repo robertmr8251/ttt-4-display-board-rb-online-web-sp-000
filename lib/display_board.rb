579270ac7faf1248816d6694d55dfa3e08e8b296
Robert Ramirez <robertmr8251@gmail.com>
2019-05-05 03:43:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+puts " #{board[0]} |   |   "
+puts "-----------"
+puts "   | #{board[4]} |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+
+
+
